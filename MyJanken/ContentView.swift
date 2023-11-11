118645dbc20ba3d020bc384ce8cd0ce1f3a97249
hujuu <1961946+hujuu@users.noreply.github.com>
2023-11-12 08:58:42 +0900

    Initial Commit

---
+++ MyJanken/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyJanken
+//
+//  Created by KEN NAKAI on 2023/11/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyJanken/MyJankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyJankenApp.swift
+//  MyJanken
+//
+//  Created by KEN NAKAI on 2023/11/12.
+//
+
+import SwiftUI
+
+@main
+struct MyJankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
