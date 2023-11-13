ce42b6cf2ce8be6dc9565f4d15b43ab5402c3022
hujuu <1961946+hujuu@users.noreply.github.com>
2023-11-13 09:04:05 +0900

    Update ContentView.swift

--- MyJanken/ContentView.swift
+++ MyJanken/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var jankenNumber = 0
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if jankenNumber == 0 {
+                Text("これから")
+                
+            } else if jankenNumber == 1 {
+                Image(.gu)
+                    .resizable()
+                    .scaledToFit()
+                Text("グー")
+            } else if jankenNumber == 2 {
+                Image(.choki)
+                    .resizable()
+                    .scaledToFit()
+                Text("チョキ")
+            } else {
+                Image(.pa)
+                    .resizable()
+                    .scaledToFit()
+                Text("パー")
+            }
+            
+            Button(action: {
+                print("タップされたよ！")
+                jankenNumber = Int.random(in: 1...3)
+            }, label: {
+                Text("じゃんけんをする！")
+            })
         }
-        .padding()
     }
 }
 
